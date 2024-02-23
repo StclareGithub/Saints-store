3b376cf9f2381c3f2d4651e59a0bac0404838b4b
StclareGithub <stclaregithuba@gmail.com>
2024-02-24 04:22:37 +0800

    final

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify:true,
+    optimizeFonts:true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: "res.cloudinary.com",
+      },
+    ],
+    minimumCacheTTL:1500000,
+  },
+}
+export default nextConfig;
